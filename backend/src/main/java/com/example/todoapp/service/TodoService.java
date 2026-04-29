9a5fa0d7d763c1b59958dcb71d8d2257844715ef
Patrick Scheibert <patrick.scheibert@outlook.com>
2026-04-29 15:44:50 +0200

    change the TodoService from interface to class to fix the tests and application spring boot context

--- backend/src/main/java/com/example/todoapp/service/TodoService.java
+++ backend/src/main/java/com/example/todoapp/service/TodoService.java
@@ -1,7 +1,14 @@
 package com.example.todoapp.service;
 
 import com.example.todoapp.dto.TodoDTO;
+import org.springframework.stereotype.Service;
 
-public interface TodoService {
-    TodoDTO create(String title);
+import java.util.UUID;
+
+@Service
+public class TodoService {
+
+    public TodoDTO create(String title) {
+        return new TodoDTO(UUID.randomUUID(), title, false);
+    }
 }
